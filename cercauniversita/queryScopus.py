45e98e2289c497474ec42266275db3a1467c53a0
Francesco Poggi <francesco.poggi5@unibo.it>
2020-02-12 13:25:30 +0100

    Test author search

--- cercauniversita/queryScopus.py
+++ cercauniversita/queryScopus.py
@@ -20,23 +20,24 @@ apiURL_search = 'https://api.elsevier.com/content/search/author'
 sector = '01/B1'
 
 pathInput = "../data/input/"
-fileCercauniversita = pathInput + "cercauniversita/" + sector.replace("/","") + "_2016.csv"
+fileCercauniversita = pathInput + "cercauniversita/" + sector.replace("/","") + "_2016_id.csv"
 #fileTsv = pathInput + "01B1_09H1_withNames_scopusId_editedAddedMissingScopusId.tsv"
+pathOutput = "../data/input/authors/" + sector.replace("/","") + "/"
 
-def searchAuthor(name, max_retry=2, retry_delay=1):
+def searchAuthor(firstname, lastname, max_retry=2, retry_delay=1):
 	
 	retry = 0
 	cont = True
 	while retry < max_retry and cont:
 
 		#print(apiURL_AbstractDoi + urllib.parse.quote(doi))
-		query = 'AUTH(' + name + ')'
+		#query = 'AUTH(' + name + ')'
+		query = 'AUTHFIRST(' + " ".join(firstname) + ') and AUTHLAST(' + " ".join(lastname) + ')'
+		print (query)
+		
 		#queryEncoded = urllib.parse.quote(query)
 		params = {'apikey':apikeys.keys[0], 'httpAccept':'application/json', 'query':query} #, 'view':'FULL'}
 		r = requests.get(apiURL_search, params=params)
-		print (r.status_code)
-		print(r.url)
-		
 			
 		#if self.raw_output:
 		#	self.save_raw_response(r.text)
@@ -60,21 +61,44 @@ def searchAuthor(name, max_retry=2, retry_delay=1):
 			 
 	if retry >= max_retry: 
 		return None 
- 
-	#json = r.json()
-	json = r.text
+		
+	json = r.json()
+	#json = r.text
 	#json['request-time'] = str(datetime.datetime.now().utcnow())
 	# TO DECODE:
 	#oDate = datetime.datetime.strptime(json['request-time'], '%Y-%m-%d %H:%M:%S.%f')
 	return json
-
+	
 with open(fileCercauniversita, newline='') as csvfile:
 	spamreader = csv.DictReader(csvfile, delimiter='\t')
 	for row in spamreader:
-		sn = (row['Cognome e Nome']).lower()
-		for c in "'-":
-			sn = sn.replace(c,"")
-		print (sn)
-		authorJson = searchAuthor(sn)
-		print (authorJson)
+		sn = (row['Cognome e Nome']).split()
+		idCercauni = row['Id']
+		
+		lastname = list()
+		firstname = list()
+		for part in sn:
+			if part.isupper():
+				lastname.append(part)
+			else:
+				firstname.append(part)
+		authorJson = searchAuthor(firstname, lastname)
+		
+		if not os.path.isdir(pathOutput):
+			os.makedirs(pathOutput)
+			
+		completepath = os.path.join(pathOutput, idCercauni + '.json')
+
+		with open(completepath, 'w') as outfile:
+			json.dump(authorJson, outfile, indent=3)
+		
+		print (authorJson['search-results']['opensearch:totalResults'])
+		
 		sys.exit()
+		
+		#for c in "'-":
+		#	sn = sn.replace(c,"")
+		#print (sn)
+		#authorJson = searchAuthor(sn)
+		#print (authorJson)
+		#sys.exit()
